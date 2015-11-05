f54b1d0a9cd67e64c8765e78fc8889a718af8280
beforexmas <raoul.sbarberi@gmail.com>
2015-11-05 10:27:32 +0100

    sperem

--- app.js
+++ app.js
@@ -8,18 +8,72 @@
  *    word_de : "Apfel"   // word in german
  *  }
  */ 
+var yes=0;
+var no=0;
 
-var tmpl = ' <li id="ID">' +
-           '  <h3>WORD</h3>' +
-           '  <h3 class="solution">SOLUTION</h3>'+
-           ' </li> ';
+var tmpl = " <li id='ID'>" +
+           "  <h3 id='inga'>WORD</h3>" +
+           "  <h3 class='solution'>SOLUTION</h3>"+
+           " </li> ";
+ 
+var i=0;
+
+function next(index) {
+      $('.options').hide();
+    $('.final').hide();  
+   
+            if (i==(data.length)) {
+                //$('.final').show();
+                $('final').text().replace('XXX',yes).replace('YYY',yes+no)
+                $('.final').show();     
+            }
+    else {
+        $('.jumbotron').append(tmpl.replace('WORD',data[i].word_en).replace('SOLUTION',data[i].word_de));
+    $('.solution').hide();
+    $('#inga').click(function() {
+        $('#inga').remove();
+        $('.solution').show();
+        $('.options').show();
+        i++;
+        $('.btnNO').click(function() {
+            no++;
+            $('#ID').remove();
+            next(i);
+        });
+        $('.btnYES').click(function() {
+            yes++;
+            $('#ID').remove();
+            next(i);
+        });
+
+    });
+
+    }    
+}
+
+
 
 $(document).ready(function(){
     $('.options').hide();
-    $('.final').hide();
-    for (var i=0;i<data.length;i++) {
-        $('jumbotron').append(tmpl).replace('WORD',);
-    }
+    $('.final').hide();     $('.jumbotron').append(tmpl.replace('WORD',data[i].word_en).replace('SOLUTION',data[i].word_de));
+    $('.solution').hide();
+    $('#inga').click(function() {
+        $('#inga').remove();
+        $('.solution').show();
+        $('.options').show();
+            i++;
+        $('.btnNO').click(function() {
+            no++;
+            $('#ID').remove();
+            next(i);
+            
+        });
+        $('.btnYES').click(function() {
+            yes++;
+            $('#ID').remove();
+            next(i);
+        });
+    });
 });
 
 
